b4d90ab78c1f650cded419acac605c837fba39c4
impelfin <impelfin@gmail.com>
2025-03-21 17:30:35 +0900

    hr-schema

--- db/shop_buytbl.sql
+++ db/shop_buytbl.sql
@@ -10,15 +10,15 @@ CREATE TABLE buytbl (
 
 CREATE SEQUENCE idseq;
 
-insert into buytbl values(idseq.nextval, 'KBS', '운동화', NULL, 30, 2);
-insert into buytbl values(idseq.nextval, 'KBS', '노트', '전자', 1000, 1);
-insert into buytbl values(idseq.nextval, 'JYP', '모니터', '전자', 200, 1);
-insert into buytbl values(idseq.nextval, 'BBK', '모니터', '전자', 200, 5);
-insert into buytbl values(idseq.nextval, 'KBS', '청바지', '의류', 50, 3);
-insert into buytbl values(idseq.nextval, 'BBK', '메모리', '전자', 80, 10);
-insert into buytbl values(idseq.nextval, 'SSK', '책', '서적', 15, 5);
-insert into buytbl values(idseq.nextval, 'EJW', '책', '서적', 15, 2);
-insert into buytbl values(idseq.nextval, 'EJW', '청바지', '의류', 50, 1);
-insert into buytbl values(idseq.nextval, 'BBK', '운동화', NULL, 30, 2);
-insert into buytbl values(idseq.nextval, 'EJW', '책', '서적', 15, 1);
-insert into buytbl values(idseq.nextval, 'BBK', '운동화', NULL, 30, 2);
+insert into shop.buytbl values(idseq.nextval, 'KBS', '운동화', NULL, 30, 2);
+insert into shop.buytbl values(idseq.nextval, 'KBS', '노트', '전자', 1000, 1);
+insert into shop.buytbl values(idseq.nextval, 'JYP', '모니터', '전자', 200, 1);
+insert into shop.buytbl values(idseq.nextval, 'BBK', '모니터', '전자', 200, 5);
+insert into shop.buytbl values(idseq.nextval, 'KBS', '청바지', '의류', 50, 3);
+insert into shop.buytbl values(idseq.nextval, 'BBK', '메모리', '전자', 80, 10);
+insert into shop.buytbl values(idseq.nextval, 'SSK', '책', '서적', 15, 5);
+insert into shop.buytbl values(idseq.nextval, 'EJW', '책', '서적', 15, 2);
+insert into shop.buytbl values(idseq.nextval, 'EJW', '청바지', '의류', 50, 1);
+insert into shop.buytbl values(idseq.nextval, 'BBK', '운동화', NULL, 30, 2);
+insert into shop.buytbl values(idseq.nextval, 'EJW', '책', '서적', 15, 1);
+insert into shop.buytbl values(idseq.nextval, 'BBK', '운동화', NULL, 30, 2);
