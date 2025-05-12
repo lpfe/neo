30d39737b484e831418f152cf4361407ea5b1a69
lpfe <pcheol0311@gmail.com>
2025-05-12 10:10:00 +0900

    RDS_docker

--- node/mysql_rds/app.js
+++ node/mysql_rds/app.js
@@ -19,17 +19,37 @@ connection.connect(function (err) {
 });
 
 app.get('/', function (req, res) {
-    connection.query('SELECT * FROM st_info', function (err, rows, fields) {
-        connection.end();
-        if (err) {
-            res.send(rews);
-            console.log("The solution is: ", rows);
+    connection.query('select * from st_info', function (err, rows, fields) {
+        // connection.end();
+        if (!err) {
+            // res.send(rows);
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            var template = `
+                <table border="1" style="margin:auto;text-align:center;">
+                <tr>
+                    <th>ST_ID</th>
+                    <th>NAME</th>
+                    <th>DEPT</th>
+                </tr>
+                `;
+            rows.forEach(item => {
+                template += `
+                    <tr>
+                        <th>${item.ST_ID}</th>
+                        <th>${item.NAME}</th>
+                        <th>${item.DEPT}</th>
+                    </tr>
+                    `;
+            });
+            template += `</table>`;
+            res.end(template);
+            console.log('The solution is : ', rows);
         } else {
-            console.log('Error while performing Query~!!\n\n');
+            console.log('Error while performing Query~!!');
         }
-    })
-})
+    });
+});
 
-app.listen(8000, function () {
-    console.log("8000 Port : Server Started~!!\n\n");
-})
+app.listen(8080, function () {
+    console.log('8080 Port : Server Started~!!');
+});
