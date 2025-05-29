c93b118a6c5d8b7c689d61ca3b9855859fd01e8f
lpfe <pcheol0311@gmail.com>
2025-05-29 01:09:33 +0000

    ai_stt_en_de

--- ai-2/3_openai/p181_app.py
+++ ai-2/3_openai/p181_app.py
@@ -1,18 +1,46 @@
+# import openai
+# from IPython.display import Image, display
+
+# org_img_file = "./data/fish.png"
+# mask_img_file = "./data/fish_mask.png"
+
+# response = openai.Image.create_edit(
+#     image = open(org_img_file, 'rb'),
+#     mask = open(mask_img_file, 'rb'),
+#     prompt = "Happy robots swimming in the water",
+#     n = 1,
+#     size = "512x512"
+# )
+
+# image_url = response['data'][0]['url']
+# print(image_url)
+# display(Image(url=image_url))
+# print("-" * 50)
+
+
+
 import openai
-from IPython.display import Image, display
+import requests
 
 org_img_file = "./data/fish.png"
 mask_img_file = "./data/fish_mask.png"
 
-response = openai.Image.create_edit(
-    image = open(org_img_file, 'rb'),
-    mask = open(mask_img_file, 'rb'),
-    prompt = "Happy robots swimming in the water",
-    n = 1,
-    size = "512x512"
-)
+with open(org_img_file, "rb") as image, open(mask_img_file, "rb") as mask:
+    response = requests.post(
+        "https://api.openai.com/v1/images/edits",
+        # headers={
+        #     "Authorization": f"Bearer {openai.api_key}"
+        # },
+        files={
+            "image": ("fish.png", image, "image/png"),
+            "mask": ("fish_mask.png", mask, "image/png")
+        },
+        data={
+            "prompt": "Happy robots swimming in the water",
+            "n": 1,
+            "size": "512x512"
+        }
+    )
 
-image_url = response['data'][0]['url']
-print(image_url)
-display(Image(url=image_url))
-print("-" * 50)
+result = response.json()
+print(result["data"][0]["url"])
