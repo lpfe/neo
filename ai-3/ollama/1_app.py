2eb39af63ec22c5454a9bcb7015f64b29abac112
pCheol <pcheol0311@gmail.com>
2025-06-09 09:22:35 +0900

    model_c

--- ai-3/ollama/1_app.py
+++ ai-3/ollama/1_app.py
@@ -3,7 +3,7 @@
 import ollama
 
 response = ollama.chat(
-    model = 'llama3.2:latest',
+    model = 'llama3.2:1b',
     messages = [
         {"role" : "system", "content" : "You are a python expert."},
         {"role" : "user", "content" : "Code a Python function to generate a Fibonacci sequence."},
